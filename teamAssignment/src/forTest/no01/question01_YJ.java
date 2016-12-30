f97f8f2a9d5a6e4dbc589c90ba53b11958b024d0
KYJ <redkar777@naver.com>
2016-12-30 16:28:13 +0900

    clear

--- teamAssignment/src/forTest/no01/question01_YJ.java
+++ teamAssignment/src/forTest/no01/question01_YJ.java
@@ -1,5 +1,14 @@
 package forTest.no01;
 
 public class question01_YJ {
-	
-}
+	public static void main(String[] args){
+		
+		String a = "";
+	for(int i= 0; i<5; i++){
+		a += "*";
+		System.out.println(a);
+		
+			}
+		}
+	}
+
