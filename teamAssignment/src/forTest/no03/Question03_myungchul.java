ad0c8c34e5abd0a201d2de50c558d37daf648b44
Administrator <Administrator@smart20111>
2016-12-30 16:53:13 +0900

    이명철 01/02번 커밋

---
+++ teamAssignment/src/forTest/no03/Question03_myungchul.java
@@ -0,0 +1,18 @@
+package forTest.no03;
+
+public class Question03_myungchul {
+
+	public static void main(String[] args) {
+		int x, y;
+
+		for (x = 0; x < 5; x++) {
+			// for(y=)
+
+		}
+		/*
+		 * 
+		 * 3번 아래그림 그리기(공백유의) 1 2 2 3 3 3 4 4 4 4 5 5 5 5 5
+		 * 
+		 */
+	}
+}
