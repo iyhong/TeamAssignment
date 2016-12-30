a60cf3d47fb6e781b3e9b05a6e0ca7aa888fa3f7
Administrator <Administrator@smart20105>
2016-12-30 16:21:13 +0900

    1번문제 해결

--- teamAssignment/src/forTest/no01/question01_choiym.java
+++ teamAssignment/src/forTest/no01/question01_choiym.java
@@ -2,5 +2,11 @@ package forTest.no01;
 
 public class question01_choiym {
 
-	
+	public static void main(String[] args){
+		String b = "*";
+		for(int a = 0; a < 5 ; a++){
+			System.out.println(b);
+			b += "*";
+		}
+	}
 }
