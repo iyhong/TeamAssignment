4937d0e3d3c2ddabd0d65b3f3ef380d5c88086d6
Administrator <Administrator@smart20105>
2016-12-30 17:26:53 +0900

    3번 잘못품 4번 품 (최유민)

--- teamAssignment/src/forTest/no03/question03_choiym.java
+++ teamAssignment/src/forTest/no03/question03_choiym.java
@@ -12,14 +12,8 @@ public class question03_choiym {
 	
 	*/
 	public static void main(String[] args){
-		int i = 0;
-		String b = "";
-		for(int a = 0; a < 5 ; a++){
-			i++;
-			b += " "+ i;
-			System.out.println(b);
-		}
 		
+	
 		
 		
 	}
---
+++ teamAssignment/src/forTest/no04/question04_choiym.java
@@ -0,0 +1,20 @@
+package forTest.no04;
+
+public class question04_choiym {
+
+	public static void main(String[] args) {
+		
+		
+	
+			int i = 0;
+			String b = "";
+			for(int a = 0; a < 5 ; a++){
+				i++;
+				b += " "+ i;
+				System.out.println(b);
+			}
+			
+			
+	}
+
+}
