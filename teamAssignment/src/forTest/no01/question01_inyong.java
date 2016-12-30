a418fe11ae15e496e2c71e563c1e56088d3427d5
Administrator <Administrator@smart20110>
2016-12-30 15:59:45 +0900

    인용정답 commit

--- teamAssignment/src/forTest/no01/question01_inyong.java
+++ teamAssignment/src/forTest/no01/question01_inyong.java
@@ -3,5 +3,11 @@ package forTest.no01;
 public class question01_inyong {
 	public static void main(String[] args){
 		//이렇게 클래서 새로 만들어서 각자 만들기~
+		for(int i=0;i<5;i++){
+			for(int j=0;j<i+1;j++){
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
 	}
 }
