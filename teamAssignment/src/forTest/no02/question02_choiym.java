f1ca16b44b6224941a51cb1198bdea54d3be79d0
Administrator <Administrator@smart20105>
2016-12-30 17:02:24 +0900

    최유민 2번문제 품

---
+++ teamAssignment/src/forTest/no02/question02_choiym.java
@@ -0,0 +1,15 @@
+package forTest.no02;
+
+public class question02_choiym {
+
+	public static void main(String[] args) {
+	for(int a =5 ; a >0; a--){
+		String c = "";
+		for(int b = 0; b < a;b++){
+			c += "*";
+		}
+		System.out.println(c);
+	}
+}
+	
+}
