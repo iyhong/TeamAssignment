a4ad02655ea01bcb071882ec4ce06decc44c7e9c
user <user@220.124.105.35>
2017-01-01 19:02:18 +0900

    최유민 5번문제 풀이 완료

--- teamAssignment/src/forTest/no02/Question02_choiym.java
+++ teamAssignment/src/forTest/no02/Question02_choiym.java
@@ -3,13 +3,13 @@ package forTest.no02;
 public class Question02_choiym {
 
 	public static void main(String[] args) {
-	for(int a =5 ; a >0; a--){
-		String c = "";
-		for(int b = 0; b < a;b++){
-			c += "*";
+		for(int a =5 ; a >0; a--){
+			String c = "";
+			for(int b = 0; b < a;b++){
+				c += "*";
+			}
+			System.out.println(c);
 		}
-		System.out.println(c);
 	}
-}
 	
 }
--- teamAssignment/src/forTest/no03/Question03_choiym.java
+++ teamAssignment/src/forTest/no03/Question03_choiym.java
@@ -14,10 +14,10 @@ public class Question03_choiym {
 	public static void main(String[] args){
 		for(int a = 1; a < 6; a ++){
 			 for(int b = 1; b <= a; b++){
-			System.out.print(a+" ");
+				 System.out.print(a+" ");
 					
-		}
+			 }
 			System.out.println();
+		}
 	}
-}
 }
