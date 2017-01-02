40884a27c2acbf98b5b3651f362ea4e361edf55f
KYJ <redkar777@naver.com>
2017-01-02 09:06:26 +0900

    Clear

---
+++ teamAssignment/src/forTest/no04/Question04_YJ.java
@@ -0,0 +1,14 @@
+package forTest.no04;
+
+public class Question04_YJ {
+	public static void main(String[] args){
+		for(int i=1; i<6; i++){
+			for(int j=1; j<i+1; j++){
+			System.out.print(j+" ");	
+			}
+			System.out.println();
+		}
+		
+		
+	}
+}
---
+++ teamAssignment/src/forTest/no05/Question05_YJ.java
@@ -0,0 +1,16 @@
+package forTest.no05;
+
+public class Question05_YJ {
+	public static void main(String[] args){
+		char a = 65;
+		for(int i=1; i<6; i++){
+			for(int j=1; j<i+1; j++){				
+				System.out.print(a++);
+			}
+			System.out.println();
+			
+			
+		}
+		
+	}
+}
