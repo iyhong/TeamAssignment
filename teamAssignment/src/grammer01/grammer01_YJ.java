c6a5bc2aae2ed41b9ae4d3c421d8ad2e5e73e8ff
KYJ <redkar777@naver.com>
2017-01-03 14:21:30 +0900

    YeaH

--- teamAssignment/src/grammer01/grammer01_YJ.java
+++ teamAssignment/src/grammer01/grammer01_YJ.java
@@ -13,12 +13,17 @@ public class grammer01_YJ {
 		int start = 1;
 		int end = 31;
 		int startWeek = THU;
-		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("SU MO TU WE TH FR SA");
 		for (int i = 1; i < startWeek; i++) {
 			System.out.print("   "); // "공백3칸"
 		}
-		for (int i = SUN, n = THU; i <= 31; i++, n++) {
-			System.out.print((i < 10) ? " 0" + i : " " + i); // (i < 10)? "공백두칸"+i : "공백한칸"+i
+		for (int i = start, n = startWeek; i <= end; i++, n++) {
+			if((i%7) == 4){
+				System.out.print((i < 10) ? "0" + i :  i);
+			}else{
+				System.out.print((i < 10) ? " 0" + i : " "+ i);
+			}
+			// (i < 10)? "공백두칸"+i : "공백한칸"+i
 			if ((n%7) == 0)	//이렇게하면 매주 토요일에는 줄바꿈이 실행됨
 				System.out.println();
 		}
