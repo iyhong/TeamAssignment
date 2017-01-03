2721f8acf4fa0875a5f843a05739538dc784959b
Administrator <Administrator@smart20106>
2017-01-03 09:15:43 +0900

    커밋

---
+++ teamAssignment/src/forTest/no02/Question02_sangil.java
@@ -0,0 +1,25 @@
+package forTest.no02;
+
+public class Question02_sangil {
+	/*
+
+2번 아래그림그리기	
+*****
+****
+***
+**
+*
+	
+ 	*/
+	public static void main(String[] args){
+		String star = "*";
+		
+		for(int i=0;i<5;i++){
+			
+			for(int j=1;j<5;j++){
+				System.out.println(star);
+			}
+			System.out.print("\n");
+		}
+	}
+}
