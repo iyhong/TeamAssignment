284abe5bbdf12fc00d5abd9fde370d8d0c345304
user <user@220.124.105.35>
2017-01-05 21:55:09 +0900

    choiym 04번문제

---
+++ teamAssignment/src/grammer04/Grammer04_choiym.java
@@ -0,0 +1,41 @@
+package grammer04;
+
+//다음 프로그램 컴파일과 실행시 출력되는 결과는?
+//컴파일시 에러가 발생한다면 라인번호와 그 원인과 해결책을 설명하시오.
+//주석달기
+public class Grammer04_choiym {
+	public static void main(String[] args) {
+		Test t1 = Test.getInstance();
+		Test t2 = Test.getInstance();
+		t1.setX(5);
+		t2.setX(10);
+		System.out.println(t1.getX());
+		System.out.println(t2.getX());
+		//Test타입의 t는 static 에 선언된 타입이므로 t1객체와 t2객체는 같은 객체를 의미하게 된다.
+		//따라서 10 10 두번 출력된다.
+	}
+}
+// 
+class Test {
+	private static Test t;
+	private int x;
+
+	private Test() {
+	}
+
+	public static Test getInstance() {
+		if (t == null) {
+			t = new Test();
+		}
+		return t;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+}
