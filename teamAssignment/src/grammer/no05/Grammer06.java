955c9eeb5669e6783a04dad646be88ede1309120
loverman85 <loverman85@hanmail.net>
2017-01-07 11:33:36 +0900

    6번문제 주석추

--- teamAssignment/src/grammer/no05/Grammer06.java
+++ teamAssignment/src/grammer/no05/Grammer06.java
@@ -1,5 +1,8 @@
 package grammer.no05;
 
+//다음 프로그램 컴파일과 실행시 출력되는 결과는?
+//컴파일시 에러가 발생한다면 라인번호와 그 원인과 해결책을 설명하시오.
+//소스 설명 주
 public class Grammer06 {
 	public static void main(String[] args) {
 		Template t = new Test();
