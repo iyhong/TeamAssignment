eff5370163305c93ddc1efeab5ddf66b4036a36f
user <user@220.124.105.35>
2017-01-09 22:54:03 +0900

    cym 10번 수정, 06번 문제 풀이

--- teamAssignment/src/forTest/no10/Question10_choiym.java
+++ teamAssignment/src/forTest/no10/Question10_choiym.java
@@ -30,22 +30,15 @@ z
 		character.add("a");
 		character.add("y");
 		
-		for(int i = 0; i < 7; i++){
+		for(int i = (character.size()-1)*(-1); i < character.size(); i++){
 			//7번 반복되는 for문 z,ze,zer,zero,zerod,zeroda,zeroday 출력할 것
-			for(int j = 0; j <= i; j++ ){
+			for(int j = 0; j < character.size()-Math.abs(i); j++ ){
+				
 				//1~7번 반복되는 for문
 				System.out.print(character.get(j));
 			}
 		System.out.println();	
 		}
-		for(int i = 6; i > 0 ; i--){
-			//6번 반복되는 for문 zeroDa,zeroD,zero,zer,ze,z 출력할 것
-			for(int j = 0; j < i ; j++){
-				//i가 6일때 j가 0~5까지 6번 반복
-				//i가 5일때 j가 0~4까지 5번 반복
-				System.out.print(character.get(j));
-			}
-			System.out.println();
-		}
+		
 	}
 }
