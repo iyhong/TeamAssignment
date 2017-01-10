50e8678c7bd3915ee6702d35bf40c809290af592
loverman85 <loverman85@hanmail.net>
2017-01-10 23:32:11 +0900

    Grammer07 문제풀이

--- teamAssignment/src/grammer/no07/Grammer07.java
+++ teamAssignment/src/grammer/no07/Grammer07.java
@@ -8,20 +8,26 @@ import java.util.*;
 */
 public class Grammer07 {
 	public static void main(String[] args) {
+		
 		Integer i1 = new Integer(1);
 		Integer i2 = new Integer(1);
-
+		
 		String s1 = new String("one");
 		String s2 = new String("one");
-
+		
 		StringBuffer sf1 = new StringBuffer("one");
 		StringBuffer sf2 = new StringBuffer("one");
 
 		Date d1 = new Date();
-		Date d2 = new GregorianCalendar(2011, 7, 15).getTime();
-		Date d3 = new GregorianCalendar(2011, 7, 15).getTime();
+		Date d2 = new GregorianCalendar(2011,07,15).getTime();
+		Date d3 = new GregorianCalendar(2011,07,15).getTime();
 		Date d4 = new Date();
-
+		
+		System.out.println(d1.getTime());
+		System.out.println(d4.getTime());
+		System.out.println(d2.getTime());
+		System.out.println(d3.getTime());
+		
 		System.out.println("1번 :"+i1.equals(i2));
 		System.out.println("2번 :"+s1.equals(s2));
 		System.out.println("3번 :"+sf1.equals(sf2));
