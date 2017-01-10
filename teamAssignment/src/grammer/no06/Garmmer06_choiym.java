35cbd265d7139317526bdb1630db4f64e8a66d74
Administrator <Administrator@smart20105>
2017-01-10 17:30:27 +0900

    cym grammer06 상속메소드 수정

--- teamAssignment/src/grammer/no06/Garmmer06_choiym.java
+++ teamAssignment/src/grammer/no06/Garmmer06_choiym.java
@@ -1,12 +1,11 @@
 package grammer.no06;
-
 //다음 프로그램 컴파일과 실행시 출력되는 결과는?
 //컴파일시 에러가 발생한다면 라인번호와 그 원인과 해결책을 설명하시오.
-
 public class Garmmer06_choiym {
 	public static void main(String[] args) {
 		Car_cym c = new Car_cym();
 		Ship_cym s = new Ship_cym();
+		System.out.println("s객체 주소: "+s);
 		Robot_cym r = new Robot_cym();
 		r.test(c);
 		r.test(s);
@@ -16,29 +15,25 @@ public class Garmmer06_choiym {
 		r.test(r);
 	}
 }
-
 class Elect_cym {
 	int value;
 }
-
 interface Testable_cym {
 }
-
 class Car_cym extends Elect_cym implements Testable_cym {
 	//Elect_cym을 상속받고 Testable_cym을 구현함
 	//int value를 상속받아 가지고있는상태 Testable_cym타입을 지니고 있는 상태
 }
-
-class Ship_cym extends Elect_cym {
+class Ship_cym extends Elect_cym implements Testable_cym{
 	//Elect_cym을 상속받아 int value를 가지고있는상태
 }
-
 class Robot_cym extends Elect_cym implements Testable_cym {
 	//Elect_cym을 상속받고 Testable_cym을 구현함
 	//int value를 상속받아 가지고있는상태 Testable_cym타입을 지니고 있는 상태
 	void test(Testable_cym t) {
+		System.out.println(t);
 		//리턴없는 test메소드 선언 매개변수로 Testable타입의 t를 받는다.
-		if (t instanceof Elect) { //instanceOf메서드는 t객체가 Elect를 참조할 수 있으면 true 없으면 false값을 리턴한다.
+		if (t instanceof Elect_cym) { //instanceOf메서드는 t객체가 Elect를 참조할 수 있으면 true 없으면 false값을 리턴한다.
 			System.out.println("test하다");
 		}
 	}
