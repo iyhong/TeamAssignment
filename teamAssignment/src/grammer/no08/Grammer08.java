3ad96afcbfb537f335996cf0e1da9863063445ff
loverman85 <loverman85@hanmail.net>
2017-01-14 19:56:38 +0900

    Grammer 8번문제

---
+++ teamAssignment/src/grammer/no08/Grammer08.java
@@ -0,0 +1,48 @@
+package grammer.no08;
+
+public class Grammer08 {
+
+/*
+ * 다음 프로그램중 컴파일시 에러가 발생하는 것은?
+	답 찾고 
+	모든 코드에 주석달아오기 오류가 나면 왜나는지 안나면 왜 안나는지?
+①
+class Parent {
+ protected void test(int x) throws Exception{}
+}
+class Child extends Parent{
+ @Override
+ void test(int x) throws Exception{} 
+}
+
+②
+class Parent {
+ protected void test(int x) throws Exception{}
+}
+class Child extends Parent{
+ @Override
+ public void test(int x) throws Exception{} 
+}
+
+③
+import java.io.*;
+class Parent {
+ protected void test(int x) throws Exception{}
+}
+class Child extends Parent{
+ @Override
+ protected void test(int x){}
+}
+
+④
+import java.io.*;
+class Parent {
+ protected void test(int x) throws Exception{}
+}
+class Child extends Parent{
+ @Override
+ protected void test(int x) throws IOException{}
+}
+ 
+*/	
+}
