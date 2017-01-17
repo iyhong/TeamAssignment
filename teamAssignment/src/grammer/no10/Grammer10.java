d124a29de4f0f1c0d32740f0c66378f65be21a73
win7 <win7@win7-PC>
2017-01-17 22:53:31 +0900

    10번문제 완료~

--- teamAssignment/src/grammer/no10/Grammer10.java
+++ teamAssignment/src/grammer/no10/Grammer10.java
@@ -12,9 +12,7 @@ public class Grammer10 {
 		st.push("0");
 		st.push("1");
 		st.push("2");
-		q.offer("0");
-		q.offer("1");
-		q.offer("2");
+	
 		while (!st.empty()) {
 			System.out.print(st.pop());
 		}
@@ -24,3 +22,4 @@ public class Grammer10 {
 		}
 	}
 }
+
