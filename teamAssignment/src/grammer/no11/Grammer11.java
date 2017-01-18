5f08e0606a7c3f7f1102bbc131fa7d54c9ef0897
loverman85 <loverman85@hanmail.net>
2017-01-18 20:58:28 +0900

    11번 문제 수정

--- teamAssignment/src/grammer/no11/Grammer11.java
+++ teamAssignment/src/grammer/no11/Grammer11.java
@@ -3,7 +3,7 @@ package grammer.no11;
 	다음 프로그램 컴파일과 실행시 출력되는 결과와 이유?
 	동적바인딩이 무엇인지?
 */
-public class Grammer11 implements Testable {
+public class Grammer11  extends B implements Testable {
 	public static void main(String[] args) {
 		Grammer11 oop = new Grammer11();
 		if (oop instanceof Object) {
@@ -12,8 +12,19 @@ public class Grammer11 implements Testable {
 		if (oop instanceof Testable) {
 			System.out.println("Testable");
 		}
+		if (oop instanceof A) {
+			System.out.println("A");
+		}
+		if (oop instanceof B) {
+			System.out.println("B");
+		}
+		if (oop instanceof C) {
+			System.out.println("C");
+		}
 	}
 }
 
-interface Testable {
-}
+interface Testable {}
+interface A{}
+class B{}
+class C{}
