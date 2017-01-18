45c4b73ddf9dc79640372299b15fdf4426b9286c
USER <USER@192.168.25.49>
2017-01-18 21:01:23 +0900

    뭐징

--- teamAssignment/src/grammer/no01/Grammer01_sangil.java
+++ teamAssignment/src/grammer/no01/Grammer01_sangil.java
@@ -2,6 +2,18 @@ package grammer.no01;
 
 public class Grammer01_sangil {
 	public static void main(String[] args) {
+		/*
+		 * [여기] 부분채워서 아래같이 출력하기
+		 * 설명주석도 달아오기
+		 
+		SU MO TU WE TH FR SA
+           			01 02 03
+ 		04 05 06 07 08 09 10
+		11 12 13 14 15 16 17
+		18 19 20 21 22 23 24
+		25 26 27 28 29 30 31
+
+		*/
 		final int SUN = 1;
 		final int MON = 2;
 		final int TUE = 3;
@@ -27,17 +39,6 @@ public class Grammer01_sangil {
 				System.out.println();
 		}
 		
-		/*
-		 * [여기] 부분채워서 아래같이 출력하기
-		 * 설명주석도 달아오기
-		 
-		SU MO TU WE TH FR SA
-           			01 02 03
- 		04 05 06 07 08 09 10
-		11 12 13 14 15 16 17
-		18 19 20 21 22 23 24
-		25 26 27 28 29 30 31
-
-		*/
+		
 	}
 }
--- teamAssignment/src/grammer/no10/Grammer10.java
+++ teamAssignment/src/grammer/no10/Grammer10.java
@@ -6,21 +6,25 @@ import java.util.*;
 */
 public class Grammer10 {
 	public static void main(String[] args) {
-		Stack st = new Stack();
+		Stack st = new Stack(); 
+								
 		Queue q = new LinkedList();
+								   
 
-		st.push("0");
-		st.push("1");
-		st.push("2");
-		q.offer("0");
-		q.offer("1");
-		q.offer("2");
-		while (!st.empty()) {
-			System.out.print(st.pop());
+		st.push("0"); 
+		st.push("1"); 
+		st.push("2"); 
+		q.offer("0"); 
+		q.offer("1"); 
+		q.offer("2"); 
+		while (!st.empty()) {			
+			System.out.print(st.pop()); 
+										
 		}
 		System.out.print("/");
-		while (!q.isEmpty()) {
-			System.out.print(q.poll());
+		while (!q.isEmpty()) {			
+			System.out.print(q.poll()); 
+										 
 		}
 	}
 }
