b46e4ee7fa676819df8bcc1fd9a3a21a01e20b75
loverman85 <loverman85@hanmail.net>
2017-01-23 22:22:12 +0900

    Algorithm 문제 01

---
+++ teamAssignment/src/algorithm/no01/Algorithm01.java
@@ -0,0 +1,15 @@
+package algorithm.no01;
+/*
+	Armstrong number  란? 
+		의 각 자릿수의 숫자 값을 ^3을 한뒤 더한값이 원래의 수와 같은 수를 말한다.
+	ex)
+	153 은 (1*1*1)+(5*5*5)+(3*3*3) = 153
+	371 은 (3*3*3)+(7*7*7)+(1*1*1) = 371
+	
+	하나의 숫자를 콘솔에서 입력받아
+	Armstrong number이면 true를 아니면 false를 콘솔에 출력하세요
+
+*/
+public class Algorithm01 {
+
+}
