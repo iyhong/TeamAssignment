5b88dc9e9e66ca66e47654800092330655bf0460
loverman85 <loverman85@hanmail.net>
2017-01-31 21:39:24 +0900

    Grammer2 02번 문제

---
+++ teamAssignment/src/grammer2/no02/Grammer202.java
@@ -0,0 +1,34 @@
+package grammer2.no02;
+/*
+	출력 : 14579
+	이 되도록 for문을 완성하세요.
+*/
+public class Grammer202 {
+	static int[] arNum = { 7, 9, 1, 4, 5 };
+
+	public static void main(String[] args) {
+		int out, in;
+		Grammer202 t = new Grammer202();
+
+		for (out = arNum.length - 1; out > 0; out--) {
+			for (;;) { // 다음 for문을 완성하시오
+				if (arNum[in] > arNum[in + 1]) {
+					t.swap(in, in + 1);
+				}
+			}
+		}
+		t.display();
+	}
+
+	void swap(int one, int two) {
+		int temp = arNum[one];
+		arNum[one] = arNum[two];
+		arNum[two] = temp;
+	}
+
+	void display() {
+		for (int index = 0; index < arNum.length; index++) {
+			System.out.print(arNum[index]);
+		}
+	}
+}
