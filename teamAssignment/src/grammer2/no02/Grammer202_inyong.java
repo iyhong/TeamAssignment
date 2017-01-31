8cce44d538b2fb2dce0bd034775cb541dd8702cb
loverman85 <loverman85@hanmail.net>
2017-01-31 22:33:23 +0900

    풀이수정

--- teamAssignment/src/grammer2/no02/Grammer202_inyong.java
+++ teamAssignment/src/grammer2/no02/Grammer202_inyong.java
@@ -8,21 +8,24 @@ package grammer2.no02;
 	그리고 각줄마다 주석을 달아 설명해보세요.
 */
 public class Grammer202_inyong {
-	static int[] arNum = { 7, 9, 1, 4, 5 };
+	static int[] arNum = { 7, 9, 5, 4, 1 };
 
 	public static void main(String[] args) {
 		int out, in;
 		Grammer202_inyong t = new Grammer202_inyong();
-
 		for (out = arNum.length - 1; out > 0; out--) {
 			for (in=0;in<out;in++) { // 다음 for문을 완성하시오
 				if (arNum[in] > arNum[in + 1]) {
 					t.swap(in, in + 1);
 				}
+				t.display();
+				System.out.println();
 			}
 		}
+		System.out.println("올림차순정렬");
 		t.display();
 		System.out.println();
+		System.out.println("내림차순정렬");
 		//반대로 정렬
 		for (out = arNum.length - 1; out > 0; out--) {
 			for (in=0;in<out;in++) { 
