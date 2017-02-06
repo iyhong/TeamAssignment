ca4e461cd4a0400af55642aae55d5e32c0658c59
user <user@user-PC>
2017-02-06 23:05:08 +0900

    문제풀이 choiym

--- teamAssignment/src/grammer2/no06/Choiym_Grammer206.java
+++ teamAssignment/src/grammer2/no06/Choiym_Grammer206.java
@@ -3,30 +3,39 @@ package grammer2.no06;
 	다음 자바 코드를 컴파일시 오류가 발생되는 이유를 적으시옹~
 */
 
-public class Choiym_Grammer206 {
+public class Grammer206 {
 	public static void main(String[] args) {
-		Parent parent = new Parent();
-		Child child = new Child();
+		Parent_choi parent = new Parent_choi();
+		Child_choi child = new Child_choi();
 		parent = child;
-		child = (Child) parent;
+		System.out.println("parent ? : "+ parent);
+		child = (Child_choi) parent;
 		parent.subMethod();
+		System.out.println("parent ? : "+ parent);
+		//child가 parent클래스 타입으로 형변환함 그러나 부모클래스에서는 subMethod가 없기때문에
+		//부모타입의 변수로는 subMethod를 사용할 수 없다. 사용하려면 child.subMethod()로 사용해야함 
 		child.superMethod();
 		System.out.println(parent.a);
+		//private 이기때문에 다른클래스에서는 a를 호출할 수 없다.
 		System.out.println(parent.b);
 		System.out.println(parent.c);
+		
 		System.out.println(parent.x);
 		System.out.println(parent.y);
 		System.out.println(parent.z);
+		//parent에서는 자식의 필드변수를 호출 할 수 없다.
 		System.out.println(child.a);
+		//private이기때문에 a가 상속이 안된상태. a가 존재하지않음
 		System.out.println(child.b);
 		System.out.println(child.c);
 		System.out.println(child.x);
+		//private이기때문에 다른클래스에서 사용할 수 없다.
 		System.out.println(child.y);
 		System.out.println(child.z);
 	}
 }
 
-class Parent {
+class Parent_choi {
 	private int a = 1;
 	protected int b = 2;
 	public int c = 3;
@@ -36,7 +45,7 @@ class Parent {
 	}
 }
 
-class Child extends Parent {
+class Child_choi extends Parent_choi {
 	private int x = 4;
 	protected int y = 5;
 	public int z = 6;
--- teamAssignment/src/grammer2/no06/Grammer206.java
+++ teamAssignment/src/grammer2/no06/Grammer206.java
@@ -8,28 +8,19 @@ public class Grammer206 {
 		Parent_choi parent = new Parent_choi();
 		Child_choi child = new Child_choi();
 		parent = child;
-		System.out.println("parent ? : "+ parent);
 		child = (Child_choi) parent;
 		parent.subMethod();
-		System.out.println("parent ? : "+ parent);
-		//child가 parent클래스 타입으로 형변환함 그러나 부모클래스에서는 subMethod가 없기때문에
-		//부모타입의 변수로는 subMethod를 사용할 수 없다. 사용하려면 child.subMethod()로 사용해야함 
 		child.superMethod();
 		System.out.println(parent.a);
-		//private 이기때문에 다른클래스에서는 a를 호출할 수 없다.
 		System.out.println(parent.b);
 		System.out.println(parent.c);
-		
 		System.out.println(parent.x);
 		System.out.println(parent.y);
 		System.out.println(parent.z);
-		//parent에서는 자식의 필드변수를 호출 할 수 없다.
 		System.out.println(child.a);
-		//private이기때문에 a가 상속이 안된상태. a가 존재하지않음
 		System.out.println(child.b);
 		System.out.println(child.c);
 		System.out.println(child.x);
-		//private이기때문에 다른클래스에서 사용할 수 없다.
 		System.out.println(child.y);
 		System.out.println(child.z);
 	}
