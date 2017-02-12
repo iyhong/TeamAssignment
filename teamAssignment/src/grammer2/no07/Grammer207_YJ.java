88d3518ee8bc2f7ab1fbe9fdaa7d068003b1ca82
win7 <win7@win7-PC>
2017-02-12 22:14:56 +0900

    문제

---
+++ teamAssignment/src/grammer2/no07/Grammer207_YJ.java
@@ -0,0 +1,58 @@
+package grammer2.no07;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+/*
+	다음 자바 코드 실행시 출력되는 결과는?
+	guest
+	guest
+	
+*/
+public class Grammer207_YJ {
+	 public static void main(String[] args) {
+		  ArrayList<Singer_yj> list=new ArrayList();
+		  Singer_yj singer;
+		  
+		  singer=new Singer_yj();
+		  
+		  singer.setName("admin");
+		  singer.setAge(30);
+		  list.add(singer);
+		  //singer 에 admin , 30 을 셋팅한다
+		  singer.setName("guest");
+		  singer.setAge(32);
+		  list.add(singer);
+		  //singer에 guest와 32를 셋팅하는데 
+		  //이 때 먼저 셋팅된 admin과 30이 덮어써지게 된다.
+		  //
+		  Iterator i=list.iterator();
+		  //Iterator는 ArrayList에 담긴 값들을 하나씩 추출할수 있게해준다.
+		  while(i.hasNext()){
+			  //i.hasNext()를 통해 담긴 값들을 하나씩 추출
+		   System.out.println((((Singer_yj)i.next()).getName()));
+		  }
+	}
+}
+
+class Singer_yj{
+	private String name;
+	private int age;
+		 
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+}
---
+++ teamAssignment/src/grammer2/no10/Grammer210_YJ.java
@@ -0,0 +1,53 @@
+package grammer2.no10;
+
+/*다음 자바 코드 실행시 출력되는 결과는?
+
+
+/*① 이름:/나이:0
+② 이름:guest/나이:0
+③ 이름:admin/나이:30
+④ Compilation error.
+⑤ Runtime exception.
+*/
+public class Grammer210_YJ {
+ public static void main(String[] args) {
+	 Sub_YJ sub=new Sub_YJ("admin",30);
+ }
+}
+class Super_YJ{
+ String name;
+ int age;
+
+ Super_YJ(){}
+ 
+ Super_YJ(String str, int val){
+  name=str;
+  age=val;
+  System.out.println("Super_YJ :"+str);
+  System.out.println("Super_YJ :"+val);
+  System.out.println("name: "+ name );
+  System.out.println("age : " + age);
+ }
+}
+class Sub_YJ extends Super_YJ{
+	Sub_YJ(String str, int val){
+		name=str;
+		age=val;
+  if(name.equals("")){
+   name="guest";
+  }
+  if(age<1 || age>200){
+   age=0;
+  }
+  System.out.println("이름:"+name+"/나이:"+age);
+ }
+}
+/* 
+ * Sub_YJ 에 값을 셋팅 하고 Sub_YJ 클래스에서 받긴 하였지만 
+ * Sub_YJ 가 상속받은 Super_YJ 클래스에 name,age 에 값이 셋팅 되지 않아서
+ * NullPointerException이 나오게되는 것 같다.
+ * name 에 str 값을 주거나 name 을 str로 바꾸면 실행된다.
+ * 
+ 
+ * */
+
