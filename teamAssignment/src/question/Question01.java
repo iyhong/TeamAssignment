6ef849b64e4a4b6929408716bebb34e81be4a1cb
user <user@user-PC>
2017-02-13 19:15:04 +0900

    choiym 문제제출

---
+++ teamAssignment/src/question/Question01.java
@@ -0,0 +1,23 @@
+package question;
+
+public class Question01 {
+
+		1. 다음 중 아래의 메서드를 올바르게 오버로딩 한 것을 모두 고르고 이유를 설명해 주세요.
+			long add(int a, int b) { return a+b;}
+			a. long add(int x, int y) { return x+y;}
+			b. long add(long a, long b) { return a+b;}
+			c. int add(byte a, byte b) { return a+b;}
+			d. int add(long a, int b) { return (int)(a+b);}
+
+	
+		2.다음 중 초기화에 대한 설명으로 옳지 않은 것을 모두 고르고 이유를 설명해 주세요.
+			a.  멤버변수는 자동 초기화되므로 초기화하지 않고도 값을 참조할 수 있다
+			b.  지역변수는 사용하기 전에 반드시 초기화해야 한다
+			c.  초기화 블럭보다 생성자가 먼저 수행된다 → 초기화 블럭이 먼저 수행된다.
+			d.  명시적 초기화를 제일 우선적으로 고려해야 한다
+			e.  클래스변수보다 인스턴스변수가 먼저 초기화된다 → 클래스변수가 먼저 초기화됨
+
+	
+	
+	
+}
